cd5fb6622fb5836ced5c2619b7056de7ee446c6e
myardor <ardormy9337@gmail.com>
2023-11-29 19:38:36 +0800

    基础文件及内容框架配置

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Footer from "./Components/Footer/Footer"
+import Home from "./Components/Home/Home"
+import Main from "./Components/Main/Main"
+import Navbar from "./Components/Navbar/Navbar"
 import "./app.css"
 
 function App() {
-  return <h2 className='App'>This is App</h2>
+  return (
+    <>
+      <Navbar />
+      {/* <Home />
+      <Main />
+      <Footer /> */}
+    </>
+  )
 }
 
 export default App
