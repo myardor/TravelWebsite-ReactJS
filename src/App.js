b161afcd86ebd6f3ea0dc68da78be500f7c19bea
myardor <ardormy9337@gmail.com>
2023-12-01 11:36:45 +0800

    Home的基础构建

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <Home />
-      <Main />
+      <Home />
+      {/* <Main />
       <Footer /> */}
     </>
   )
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,52 @@
 import React from "react"
 import "./home.css"
+import video from "../../assets/video.mp4"
+import { CiLocationOn } from "react-icons/ci"
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <section className='home'>
+      <div className='overlay'></div>
+      <video src={video} type='video/mp4' autoPlay muted loop></video>
+
+      <div className='homeContent container'>
+        <div className='textDiv'>
+          <span className='smallText'>Our Packages</span>
+          <h1 className='homeTitle'>Search your Holiday</h1>
+        </div>
+
+        <div className='cardDiv grid'>
+          {/* 目的地 */}
+          <div className='destinationInput'>
+            <label htmlFor='city'>Search your destination:</label>
+            <div className='input flex'>
+              <input type='text' placeholder='Enter name here...' />
+              <CiLocationOn className='icon' />
+            </div>
+          </div>
+          {/* 日期 */}
+          <div className='dateInput'>
+            <label htmlFor='date'>Select your date:</label>
+            <div className='input flex'>
+              <input type='date' />
+            </div>
+          </div>
+
+          {/* 预算 */}
+          <div className='priceInput'>
+            <div className='label_total flex'>
+              <label htmlFor='price'>Max price:</label>
+              <h3>$5000</h3>
+            </div>
+
+            <div className='input flex'>
+              <input type='range' max='5000' min='1000' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Home
