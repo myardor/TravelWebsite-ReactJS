0c4611cd1ba49e3c1b1e0b718cf17dee7e4dea1e
myardor <ardormy9337@gmail.com>
2023-12-04 17:05:40 +0800

    新增Main页change按钮实现更换目的地的效果

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react"
+import "./main.css"
+
+import Aos from "aos"
+import "aos/dist/aos.css"
+import _ from "lodash"
+
+// import icons
+import { FaLocationDot } from "react-icons/fa6"
+import { LuClipboardPaste } from "react-icons/lu"
+import axios from "axios"
+
+const Main = () => {
+  const [Data, setData] = useState([])
+  const [currentData, setCurrentData] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(3)
+
+  useEffect(async () => {
+    const res = await axios({
+      url: "http://localhost:3003/Data",
+    })
+    setData(res.data)
+    setCurrentData(res.data.slice(0, 3))
+  }, [])
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 })
+  }, [])
+
+  const onClickChange = () => {
+    if (currentIndex === 15) {
+      setCurrentIndex(0)
+    } else {
+      setCurrentIndex(currentIndex + 3)
+    }
+    setCurrentData(Data.slice(currentIndex, currentIndex + 3))
+  }
+
+  return (
+    <section className='main container section'>
+      <div className='secTitle'>
+        <h3 data-aos='fade-right' className='title'>
+          Most visited destinations
+        </h3>
+        <button className='btn' onClick={onClickChange}>
+          CHANGE
+        </button>
+      </div>
+      <div className='secContent grid'>
+        {currentData.map(
+          ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+            return (
+              <div key={id} data-aos='fade-up' className='singleDestination'>
+                <div className='imageDiv'>
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+
+                <div className='cardInfo'>
+                  <h4 className='destTitle'>{destTitle}</h4>
+
+                  <span className='continent flex'>
+                    <FaLocationDot className='icon' />
+                    <span className='name'>{location}</span>
+                  </span>
+
+                  {/* 花费 */}
+                  <div className='fees flex'>
+                    <div className='grade'>
+                      <span>
+                        {grade}
+                        <small>+1</small>
+                      </span>
+                    </div>
+                    <div className='price'>
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+
+                  {/* 景点描述 */}
+                  <div className='desc'>
+                    <p>{description}</p>
+                  </div>
+
+                  {/* 了解详情 */}
+                  <button className='btn flex'>
+                    Details
+                    <LuClipboardPaste className='icon' />
+                  </button>
+                </div>
+              </div>
+            )
+          }
+        )}
+      </div>
+    </section>
+  )
+}
+
+export default Main
