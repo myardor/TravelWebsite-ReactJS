55ea5d015a930f1a4af5044dfb3852f9e86ea261
myardor <ardormy9337@gmail.com>
2023-12-14 16:29:52 +0800

    懒加载实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from "react-router-dom"
+import { Suspense, lazy } from "react"
 
-import Login from "../pages/Login/Login"
-import Layout from "../pages/Layout"
-import Stay from "../pages/Stay/Stay"
-import Index from "../pages/Index"
-import Flight from "../pages/Flight/Flight"
-import Attraction from "../pages/Attraction/Attraction"
+// import Login from "../pages/Login/Login"
+// import Layout from "../pages/Layout"
+// import Stay from "../pages/Stay/Stay"
+// import Index from "../pages/Index"
+// import Flight from "../pages/Flight/Flight"
+// import Attraction from "../pages/Attraction/Attraction"
+
+const Index = lazy(() => import("../pages/Index"))
+const Login = lazy(() => import("../pages/Login/Login"))
+const Layout = lazy(() => import("../pages/Layout"))
+const Stay = lazy(() => import("../pages/Stay/Stay"))
+const Flight = lazy(() => import("../pages/Flight/Flight"))
+const Attraction = lazy(() => import("../pages/Attraction/Attraction"))
 
 const router = createBrowserRouter([
   {
@@ -14,25 +22,45 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Index />,
+        element: (
+          <Suspense fallback={"加载中"}>
+            <Index />
+          </Suspense>
+        ),
       },
       {
         path: "stay",
-        element: <Stay />,
+        element: (
+          <Suspense fallback={"加载中"}>
+            <Stay />
+          </Suspense>
+        ),
       },
       {
         path: "flight",
-        element: <Flight />,
+        element: (
+          <Suspense fallback={"加载中"}>
+            <Flight />
+          </Suspense>
+        ),
       },
       {
         path: "attraction",
-        element: <Attraction />,
+        element: (
+          <Suspense fallback={"加载中"}>
+            <Attraction />
+          </Suspense>
+        ),
       },
     ],
   },
   {
     path: "/login",
-    element: <Login />,
+    element: (
+      <Suspense fallback={"加载中"}>
+        <Login />
+      </Suspense>
+    ),
   },
 ])
 
